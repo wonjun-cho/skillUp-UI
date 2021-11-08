bf3e2c5ac41c32016f5ee1f8919c426f2a0196f4
WonJun.Cho <wonjun.cho@cognizant.com>
2021-11-07 23:31:12 -0600

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
+import { ProjectComponent } from './project/project.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TaskComponent,
+    ProjectComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
